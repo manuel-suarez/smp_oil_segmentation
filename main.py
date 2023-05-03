20ab46903feca947f8076c79dfaa66cdf14e4e15
Arturo Suárez <manuel.suarez@cimat.mx>
2023-05-03 14:08:59 -0600

    Configure dataset

--- main.py
+++ main.py
@@ -13,6 +13,42 @@ from segmentation_models_pytorch.datasets import SimpleOxfordPetDataset
 logging.basicConfig(filename='app.log', filemode='w', format='%(name)s %(levelname)s - %(message)s', level=logging.INFO)
 # download data
 root = "."
-SimpleOxfordPetDataset.download(root)
+#SimpleOxfordPetDataset.download(root)
+
+# init train, val, test sets
+train_dataset = SimpleOxfordPetDataset(root, "train")
+valid_dataset = SimpleOxfordPetDataset(root, "valid")
+test_dataset = SimpleOxfordPetDataset(root, "test")
+
+logging.info(f"Train size: {len(train_dataset)}")
+logging.info(f"Valid size: {len(valid_dataset)}")
+logging.info(f"Test size: {len(test_dataset)}")
+
+n_cpu = os.cpu_count()
+train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=n_cpu)
+valid_dataloader = DataLoader(valid_dataset, batch_size=16, shuffle=False, num_workers=n_cpu)
+test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=n_cpu)
+
+# Samples
+sample = train_dataset[0]
+plt.subplot(1,2,1)
+plt.imshow(sample["image"].transpose(1, 2, 0)) # for visualization we have to transpose back to HWC
+plt.subplot(1,2,2)
+plt.imshow(sample["mask"].squeeze())  # for visualization we have to remove 3rd dimension of mask
+plt.savefig("figure1.png")
+
+sample = valid_dataset[0]
+plt.subplot(1,2,1)
+plt.imshow(sample["image"].transpose(1, 2, 0)) # for visualization we have to transpose back to HWC
+plt.subplot(1,2,2)
+plt.imshow(sample["mask"].squeeze())  # for visualization we have to remove 3rd dimension of mask
+plt.savefig("figure2.png")
+
+sample = test_dataset[0]
+plt.subplot(1,2,1)
+plt.imshow(sample["image"].transpose(1, 2, 0)) # for visualization we have to transpose back to HWC
+plt.subplot(1,2,2)
+plt.imshow(sample["mask"].squeeze())  # for visualization we have to remove 3rd dimension of mask
+plt.savefig("figure3.png")
 
 logging.info('Done!')
