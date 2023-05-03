1fe4fce271db4bccf28acd6547cfd0d16b5a1bc1
Arturo Suárez <manuel.suarez@cimat.mx>
2023-05-03 14:20:40 -0600

    Image's dimensions

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ plt.imshow(sample["image"].transpose(1, 2, 0)) # for visualization we have to tr
 plt.subplot(1,2,2)
 plt.imshow(sample["mask"].squeeze())  # for visualization we have to remove 3rd dimension of mask
 plt.savefig("figure1.png")
+logging.info(f"Train image size: {sample['image'].size}")
 
 sample = valid_dataset[0]
 plt.subplot(1,2,1)
@@ -43,6 +44,7 @@ plt.imshow(sample["image"].transpose(1, 2, 0)) # for visualization we have to tr
 plt.subplot(1,2,2)
 plt.imshow(sample["mask"].squeeze())  # for visualization we have to remove 3rd dimension of mask
 plt.savefig("figure2.png")
+logging.info(f"Valid image size: {sample['image'].size}")
 
 sample = test_dataset[0]
 plt.subplot(1,2,1)
@@ -50,5 +52,6 @@ plt.imshow(sample["image"].transpose(1, 2, 0)) # for visualization we have to tr
 plt.subplot(1,2,2)
 plt.imshow(sample["mask"].squeeze())  # for visualization we have to remove 3rd dimension of mask
 plt.savefig("figure3.png")
+logging.info(f"Test image size: {sample['image'].size}")
 
 logging.info('Done!')
