2cdf98ea4af15e3522f19f20e99bb0bf076fb181
Arturo Suárez <manuel.suarez@cimat.mx>
2023-05-03 12:53:54 -0600

    Logging

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import logging
 import matplotlib.pyplot as plt
 import pytorch_lightning as pl
 import segmentation_models_pytorch as smp
@@ -9,7 +10,9 @@ from torch.utils.data import DataLoader
 
 from segmentation_models_pytorch import SimpleOxfordPetDataset
 
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s %(levelname)s - %(message)s', level=logging.INFO)
 # download data
 root = "."
 SimpleOxfordPetDataset.download(root)
 
+logging.info('Done!')
