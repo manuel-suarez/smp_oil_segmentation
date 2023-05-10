30b9d3415baa718608bdeabe618c14df851b6750
Arturo Suárez <manuel.suarez@cimat.mx>
2023-05-10 11:49:40 -0600

    Verify shapes

--- utils.py
+++ utils.py
@@ -8,13 +8,17 @@ def save_figure(dataset, name, figname):
     sample = dataset[5]
 
     plt.subplot(1, 2, 1)
-    image = sample["image"].transpose(1, 2, 0)
+    image = sample["image"]
     logging.info(f"Image shape: {image.shape}")
+    image = image.transpose(1, 2, 0)
+    logging.info(f"Image transpose shape: {image.shape}")
     plt.imshow(image)  # for visualization we have to transpose back to HWC
     plt.subplot(1, 2, 2)
-    mask = sample["mask"].squeeze()
+    mask = sample["mask"]
     logging.info(f"Mask shape: {mask.shape}")
+    mask = mask.squeeze()
+    logging.info(f"Mask squeeze shape: {mask.shape}")
     # Display first mask only
-    plt.imshow(mask[0])  # for visualization we have to remove 3rd dimension of mask
+    plt.imshow(mask)
     plt.savefig(figname)
     logging.info(f"{name} image shape: {sample['image'].shape}")
